92945c208ead5f4cdcc4dde974dd45fbc6479b6b
Karim Dilling <ktk.dilling@freenet.de>
2021-09-02 17:57:09 +0200

    Removed unnecessary positional argument of grid constructor

--- game_window.py
+++ game_window.py
@@ -6,9 +6,9 @@ class GameWindow:
     def __init__(self):
         self.screen = pygame.display.set_mode((600, 800))
         pygame.display.set_caption("Tetris")
-        self.grid = Grid((0, 0), 16, 10, 30)
+        self.grid = Grid(16, 10, 30)
 
     def draw(self):
         self.screen.fill("blue")
-        self.screen.blit(self.grid.surface, (100, 50))
+        self.screen.blit(self.grid.surface, (50, 50))
         self.grid.draw()
--- grid.py
+++ grid.py
@@ -2,18 +2,18 @@ import pygame
 
 
 class Grid:
-    def __init__(self, pos, rows, cols, tile_size):
+    def __init__(self, rows, cols, tile_size):
         self.surface = pygame.Surface((cols * tile_size, rows * tile_size))
-        self.rect = self.surface.get_rect(topleft=pos)
+        self.rect = self.surface.get_rect()
         self.rows = rows
         self.cols = cols
         self.tile_size = tile_size
 
     def draw(self):
-        for row in range(1, self.rows + 1):
+        for row in range(1, self.rows):
             y = row * self.tile_size
             pygame.draw.line(self.surface, "grey", (0, y), (self.surface.get_width(), y))
-        for col in range(1, self.cols + 1):
+        for col in range(1, self.cols):
             x = col * self.tile_size
             pygame.draw.line(self.surface, "grey", (x, 0), (x, self.surface.get_height()))
         pygame.draw.rect(self.surface, "grey", self.rect, 1)
